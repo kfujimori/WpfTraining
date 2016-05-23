dc9c32023716e25c9b70745bc72c64bcb1531339
kfujimori <atiger7391@gmail.com>
2016-05-24 01:15:29 +0900

    add button event and add dataGrid

--- WpfTraining/MainWindow.xaml.cs
+++ WpfTraining/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Collections.ObjectModel;
 
 namespace WpfTraining
 {
@@ -23,6 +25,32 @@ namespace WpfTraining
         public MainWindow()
         {
             InitializeComponent();
+
+            var data = new ObservableCollection<Person>(
+                Enumerable.Range(1, 100).Select(i => new Person
+                {
+                    Name = "あああ" + i,
+                    Gender = i % 2 == 0 ? Gender.men : Gender.women,
+                    Age = 20 + i % 50,
+                    AuthMember = i % 5 == 0
+                })
+                
+            );
+            this.dataGrid.ItemsSource = data;
+        }
+
+        private int count = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            button.Content = string.Format("{0}回", ++count);
+        }
+
+        private int count2 = 0;
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (RepeatButton)sender;
+            button.Content = string.Format("{0}回", ++count2);
         }
     }
 }
---
+++ WpfTraining/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTraining
+{
+
+    public enum Gender
+    {
+        none,
+        men,
+        women
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public bool AuthMember { get; set; }
+    }
+}
