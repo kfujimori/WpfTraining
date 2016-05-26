cb556e8bbdd5480cb43473a534f15e2424605121
kfujimori <atiger7391@gmail.com>
2016-05-26 23:57:23 +0900

    add gender combobox with japanese text

---
+++ WpfTraining/GenderComboBoxItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTraining
+{
+    public class GenderComboBoxItem
+    {
+        public string Label { get; set; }
+        public Gender Value { get; set; }
+    }
+}
