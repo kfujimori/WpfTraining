930f85e637b8b98925df8287ccc8773089f220b8
kfujimori <atiger7391@gmail.com>
2016-06-05 23:50:05 +0900

    add toolbar, menubar, checkbox

--- WpfTraining_Menu/WpfTraining_Menu/MainWindow.xaml.cs
+++ WpfTraining_Menu/WpfTraining_Menu/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace WpfTraining_Menu
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            TabControl1.Items.Add(new TabItem()
+            {
+                Header = "added tab"
+            });
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            TabControl1.Items.Remove(TabControl1.Items.GetItemAt(TabControl1.SelectedIndex));
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.textBlock.Text = "on";
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.textBlock.Text = "off";
+        }
+
+        private void CheckBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            this.textBlock.Text = "others";
+        }
     }
 }
