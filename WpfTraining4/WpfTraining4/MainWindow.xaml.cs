f38998fb5aa4e85632aa8b547fe76256ed9cac85
kfujimori <atiger7391@gmail.com>
2016-06-12 17:01:40 +0900

    add slider,tabControl,fileDialog,Label

--- WpfTraining4/WpfTraining4/MainWindow.xaml.cs
+++ WpfTraining4/WpfTraining4/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WpfTraining4
 {
@@ -23,6 +24,52 @@ namespace WpfTraining4
         public MainWindow()
         {
             InitializeComponent();
+
+            var source = Enumerable.Range(1, 10)
+                .Select(i => new Person { Name = "P" + i, Age = 10 + i });
+            this.tabControl.ItemsSource = source;
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            if( slider != null)
+            {
+                double rounded = Math.Round(slider.Value, 1);
+                slider.Value = rounded;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "open file";
+            dialog.Filter = "all files(*.*)|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                this.textBlockFileName.Text = System.IO.Path.GetFileName(dialog.FileName.ToString());
+            }
+            else
+            {
+                this.textBlockFileName.Text = "canceled";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Title = "save file";
+            dialog.Filter = "all files(*.*)|*.*";
+            if(dialog.ShowDialog() == true)
+            {
+                this.textBlockFileName.Text = System.IO.Path.GetFileName(dialog.FileName.ToString());
+            }
+            else
+            {
+                this.textBlockFileName.Text = "canceled";
+            }
+
         }
     }
 }
